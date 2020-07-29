d24f126491f556fa250e86816da16b49836af7fb
kaushty <kaushikshetty4898@gmail.com>
2020-07-29 15:55:24 +0530

    Initial Commit
    modules structured in appropriate  folders.

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Styles/App.css';
+import '../Styles/root_style.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1 className='app-title'>Would You Rather ?</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
